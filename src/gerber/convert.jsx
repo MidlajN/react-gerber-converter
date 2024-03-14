ff94548a4553f6fb171ed7dab35db673a377c616
MidlajN <midlaj4n@gmail.com>
2024-03-14 21:16:14 +0530

    Functionality : Svg Conversion Finished

---
+++ src/gerber/convert.jsx
@@ -0,0 +1,105 @@
+
+
+async function convertToSvg(files, setTopStack, setBottomStack, setFullLayers) {
+    const fullStackSvg = useGerberToSvg(files, stackup.layers, stackup.top)
+    setFullLayers(fullStackSvg)
+
+    const stackup = await useStackup(files)
+    const topxmlDoc = new DOMParser().parseFromString(stackup.top.svg, 'image/svg+xml');
+    const topsvg = topxmlDoc.documentElement;
+    setTopStack(topsvg)
+
+    const bottomxmlDoc = new DOMParser().parseFromString(stackup.bottom.svg, 'image/svg+xml');
+    const bottomsvg = bottomxmlDoc.documentElement;
+    setBottomStack(bottomsvg)
+}
+
+export default convertToSvg
+
+
+function useStackup(filesList) {
+    return Promise.all(
+        Array.from(filesList).map(file => {
+            const reader = new FileReader();
+            return new Promise((resolve, reject) => {
+            reader.onload = ({ target: { result: fileContent } }) =>
+                resolve({ filename: file.name, gerber: fileContent });
+            reader.onerror = error => reject(error);
+            reader.readAsText(file);
+            });
+        })
+    )
+  .then(layers => pcbStackup(layers))
+  .catch(error => console.error(error));
+}
+
+
+function useGerberToSvg(files, layers, svgData) {
+    const ids = layers.map(({side, type}) => `${side}_${type}`);
+
+    const svg = svgData.svg;
+    const svgDoc = new DOMParser().parseFromString(svg, 'image/svg+xml');
+    const rootGElement = svgDoc.documentElement.querySelector('svg > g');
+    const gTransform = rootGElement.getAttribute('transform');
+
+    const fullLayerSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    for (const [key, value] of Object.entries(svgData.attributes)) {
+        fullLayerSvg.setAttribute(key, value);
+    }
+
+    const fullLayerDef = document.createElementNS('http://www.w3.org/2000/svg', 'defs');
+    const fullLayerG = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+    fullLayerG.setAttribute('transform', gTransform);
+
+    Array.from(files).forEach((file, index) => {
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+            const fileContent = e.target.result;
+            const uint8Array = new Uint8Array(fileContent);
+
+            let gerberToSvgStream = gerberToSvg(uint8Array);
+            let svg = ''
+
+            gerberToSvgStream.on('data', (chunk) => { svg += chunk; });
+
+            gerberToSvgStream.on('end', () => {
+                const svgDoc = new DOMParser().parseFromString(svg, 'image/svg+xml');
+
+                const defElement = svgDoc.querySelector('defs');
+                if (defElement) {
+                    fullLayerDef.appendChild(defElement);
+                }
+
+                const gElement = svgDoc.querySelector('g');
+                if (gElement) {
+                    gElement.setAttribute('id', `g-${ids[index]}`);
+                    gElement.removeAttribute('transform');
+
+                    const layerStyle = {
+                        'top_copper': {color: 'crimson', opacity: 0.3},
+                        'bottom_copper': {color: '#008208', opacity: 0.3},
+                        'all_outline': {color: 'green', opacity: 0.5},
+                        'top_silkscreen': {color: 'red', opacity: 0.5},
+                        'bottom_silkscreen': {color: 'blue', opacity: 0.5},
+                        'bottom_soldermask': {color: '#757500', opacity: 0.5, display: 'none'},
+                        'bottom_solderpaste': {color: 'orange', opacity: 0.5},
+                        'top_solderpaste': {color: '#c362c3', opacity: 0.5},
+                        'top_soldermask': {color: '#af4e5f', opacity: 0.5, display: 'none'},
+                    };
+
+                    const layerstyle = layerStyle[ids[index]] || { color: 'green', opacity: 0.5 };
+                    gElement.setAttribute('style', `color: ${layerstyle.color}; opacity: ${layerstyle.opacity};`);
+                    fullLayerG.appendChild(gElement);
+                }
+            })
+        }
+
+        reader.readAsArrayBuffer(file);
+    });
+
+    fullLayerSvg.appendChild(fullLayerDef);
+    fullLayerSvg.appendChild(fullLayerG);
+
+    return fullLayerSvg
+}
--- src/gerber/gerber.jsx
+++ src/gerber/gerber.jsx
@@ -1,11 +1,42 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import ConfigSection from "./configSection.jsx"
 import './gerber.css'
 import pcbLogo from '../assets/pcbLogo.png'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import convertToSvg from "./convert.jsx";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+
 
 function GerberSection() {
+    const [isDragging, setIsDragging] = useState(false);
+    const [topstack, setTopStack] = useState(null);
+    const [bottomstack, setBottomStack] = useState(null);
+    const [fullLayers, setFullLayers] = useState(null);
+
     const resultRef = useRef(null);
+    const dropAreaRef = useRef(null);
+
+    useEffect(() => {
+
+        if (topstack) {
+            resultRef.current.innerHTML = '';
+
+            resultRef.current.appendChild(topstack)
+            // resultRef.current.appendChild(bottomstack)
+        }
+
+    }, [topstack, bottomstack])
+
+    const handleInputFiles = (e) => {
+        e.preventDefault();
+
+        // Access the Files From DataTransfer Object if the files are dropped
+        const files = e.dataTransfer ? e.dataTransfer.files : e.target.files;
+        convertToSvg(files, setTopStack, setBottomStack, setFullLayers).then(() => {
+            dropAreaRef.current.style.display = 'none';
+        })
+    }
+    
     return (
         <>
             <div className="relative h-[90%] " >
@@ -14,32 +45,56 @@ function GerberSection() {
                 </div>
                 <div className="h-full flex items-center justify-end">
                     <div className="w-4/5 h-full gridsection">
-                        <div id="dropArea" className="dropArea" style={{'display': 'flex'}}>
+                        <div 
+                            ref={ dropAreaRef }
+                            className={`dropArea ${isDragging ? 'active' : ''}`} 
+                            onDragOver={(e) => { e.preventDefault() }} 
+                            onDragEnter={(e) => { e.preventDefault(); setIsDragging(true) }} 
+                            onDragLeave={(e) => { e.preventDefault(); setIsDragging(false) }} 
+                            onDrop={ handleInputFiles }
+                        >
                             <div className="dropbox">
                                 <div className="shadow">
                                     <p>Drop your Gerber file here</p>
-                                    <input type="file" id="gerberFileInput" multiple/>
+                                    <input type="file" id="gerberFileInput" onChange={ handleInputFiles } multiple/>
                                 </div>
                             </div>
                         </div>
-                        <div id="result" ref={resultRef} >
-                            <div className="pngDiv">
-                                <div className="png">
-                                    <img className="w-full" src={ pcbLogo } alt="" />
-                                    <div className="footer">
-                                        <span>new_gerber_png.png</span>
-                                        <div className="buttons gap-2 flex">
-                                            <a href="">
-                                                <FontAwesomeIcon icon="fa-solid fa-download" />
-                                            </a>
-                                            <a href="">
-                                                <FontAwesomeIcon icon="fa-solid fa-trash" />
-                                            </a>
-                                        </div>
+
+                        <TransformWrapper 
+                            initialScale={1} 
+                            minScale={.5} 
+                            limitToBounds={false }
+                        >
+                            <TransformComponent
+                                contentStyle={{  margin:'auto', transition: 'transform 0.3s ease' }} 
+                                wrapperStyle={{ width: '100%', height: '100%', overflow:'visible', display:'flex'}} 
+                            >
+                                <div ref={ resultRef } className="flex items-center h-full justify-center"></div>
+                            </TransformComponent>
+                        </TransformWrapper>
+
+
+
+
+
+
+                        {/* <div className="pngDiv">
+                            <div className="png">
+                                <img className="w-full" src={ pcbLogo } alt="" />
+                                <div className="footer">
+                                    <span>new_gerber_png.png</span>
+                                    <div className="buttons gap-2 flex">
+                                        <a href="">
+                                            <FontAwesomeIcon icon="fa-solid fa-download" />
+                                        </a>
+                                        <a href="">
+                                            <FontAwesomeIcon icon="fa-solid fa-trash" />
+                                        </a>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
